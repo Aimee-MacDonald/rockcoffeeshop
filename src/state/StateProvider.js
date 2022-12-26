a7ea93724cdb75bd4b5ed18f159d2ec7eff9b658
Aimee MacDonald <aimeelmacdonald@gmail.com>
2022-12-26 12:49:06 +0200

    Code layout

--- src/state/StateProvider.js
+++ src/state/StateProvider.js
@@ -4,31 +4,14 @@ export default ({ children }) => {
   const [ orders, setOrders ] = useState([])
   const [ activeOrder, setActiveOrder ] = useState(null)
 
-  const products = [{
-    id: 0,
-    name: 'Espresso',
-    price: 30
-  }, {
-    id: 1,
-    name: 'Afrikano',
-    price: 30
-  }, {
-    id: 2,
-    name: 'Red Chino',
-    price: 40
-  }, {
-    id: 3,
-    name: 'Cappuccino',
-    price: 45
-  }, {
-    id: 4,
-    name: 'Latte',
-    price: 45
-  }, {
-    id: 5,
-    name: 'Kickass',
-    price: 75
-  }]
+  const products = [
+    { id: 0, name: 'Espresso', price: 30 },
+    { id: 1, name: 'Afrikano', price: 30 },
+    { id: 2, name: 'Red Chino', price: 40 },
+    { id: 3, name: 'Cappuccino', price: 45},
+    { id: 4, name: 'Latte', price: 45 },
+    { id: 5, name: 'Kickass', price: 75}
+  ]
 
   const state = {
     products,
