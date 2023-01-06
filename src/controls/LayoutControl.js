027ed5330950e2427039c1500f3e92cd475cee9c
Aimee MacDonald <aimeelmacdonald@gmail.com>
2023-01-06 15:23:53 +0200

    Flappy Layout Control

--- src/controls/LayoutControl.js
+++ src/controls/LayoutControl.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const LayoutControl = () => (
-  <StyledLayoutControl>
-    <button type='button'>Orders</button>
-    <button type='button'>Expenses</button>
-  </StyledLayoutControl>
-)
+const LayoutControl = () => {
+  const [ open, setOpen ] = useState(false)
+
+  return (
+    <StyledLayoutControl 
+      open={ open } 
+      onMouseEnter={ () => setOpen(true) }
+      onMouseLeave={ () => setOpen(false) }>
+      {open && <button type='button'>Orders</button>}
+      {open && <button type='button'>Expenses</button>}
+    </StyledLayoutControl>
+  )
+}
 
 const StyledLayoutControl = styled.div`
-  background-color: red;
-  width: 16vw;
+  background-color: ${ ({ open }) => open ? 'red' : 'transparent' };
+  width: ${ ({ open }) => open ? '16vw' : '2vw' };
   height: 100vh;
   display: flex;
   flex-flow: nowrap column;
+  transition: 0.2s;
 `
 
 export default LayoutControl
